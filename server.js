500ff0c924b63e5f6ca20ecf7967af27c5b0e983
dtopal <11358089+dtopal@users.noreply.github.com>
2019-11-14 12:23:32 +0900

    Check if url is the correct format and a valid hostname

--- server.js
+++ server.js
@@ -3,21 +3,26 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
+var url = require('url');
+var valid = require('valid-url');
 
 var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,8 +30,37 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+// urlshortner API
+app.post("/api/shorturl/new", function (req, res) {
+  var uri = req.body.url;
+
+  //format check
+  if (valid.isWebUri(uri) === undefined) {
+    res.json({ "error": "invalid URL" });
+    return;
+  }
+
+  //check if hostname is valid
+  var curr = url.parse(uri);
+  var hostname = curr.hostname;
+
+  var options = { all: true };
+  dns.lookup(hostname, options, function(err, address, family) {
+    if (err) {
+      res.json({ "error": "invalid URL" });
+      console.error(err);
+    } else {
+      console.log(address);
+      res.json({ "url": req.body.url });
+    }
+
+  });
+
+
+  //res.json({ "url": req.body.url });
+});
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
